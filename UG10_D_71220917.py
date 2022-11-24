0c8c81c30c97c55976e7b3b0546070a4d5bf2b53
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2022-11-24 16:08:35 +0700

    Add files via upload

---
+++ UG10_D_71220917.py
@@ -0,0 +1,25 @@
+print("selamat datang kalkulator sederhana.")
+print("ketik 1 untuk menghitung penjumlahan.")
+print("ketik 2 untuk menghitung pengurangan.")
+print("ketik 3 untuk menghitung perkalian.")
+print("ketik 4 untuk menghitung pembagian.")
+print("ketik 5 untuk menghitung sisa hasil bagi(modulus).")
+print("ketik 6 untuk menghitung pemangkatan.")
+pilihan = input("masukan pilhan anda: ")
+pilhanbil = input("masukan bilangan pertama: ")
+pilhanbil3= input("masukkan bilangan kedua: ")
+
+print("selamat datang kalkulator sederhana.")
+print("ketik 1 untuk menghitung penjumlahan.")
+print("ketik 2 untuk menghitung pengurangan.")
+print("ketik 3 untuk menghitung perkalian.")
+print("ketik 4 untuk menghitung pembagian.")
+print("ketik 5 untuk menghitung sisa hasil bagi(modulus).")
+print("ketik 6 untuk menghitung pemangkatan.")
+pilihan = input("masukan pilhan anda: ")
+pilhanbil = input("masukan bilangan pertama: ")
+pilhanbil3= input("masukkan bilangan kedua: ") 
+
+
+
+nn = input("masukkan tahun:")
